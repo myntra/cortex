14c2352b02f65eab70cd62dbdc86c391c3c52b3f
16533 <sayam.jain@myntra.com>
2018-07-25 17:08:12 +0530

    Changed the test casese

--- pkg/events/sinks/sink_test.go
+++ pkg/events/sinks/sink_test.go
@@ -8,12 +8,12 @@ import (
 )
 
 var alert = types.Site247Alert{
-	MonitorName:          "nike_test",
+	MonitorName:          "brand_test",
 	MonitorGroupName:     "search",
 	SearchPollFrequency:  1,
 	MonitorID:            2136797812307,
 	FailedLocations:      "Delhi,Bangalore",
-	MonitorURL:           "https://www.myntra.com/search?query=Nike",
+	MonitorURL:           "https://localhost:4000/search?query=Nike",
 	IncidentTimeISO:      "2018-07-24T18:43:08+0530",
 	MonitorType:          "URL",
 	Status:               "DOWN",
@@ -21,7 +21,7 @@ var alert = types.Site247Alert{
 	IncidentTime:         "July 24, 2018 6:43 PM IST",
 	IncidentReason:       "Host Unavailable",
 	OutageTimeUnixFormat: 1532437988741,
-	RCALink:              "https://www.site24x7.com/rca.do?id=ygaPwszXNqar1O9ZCR3WWw6HsI%2BteZf8Tj7KEfQd5raoQbNDuhxUt%2FQoG0yksN9DXHqtLUl5%2FcRr%0AtIcRB15%2Bxw%3D%3D",
+	RCALink:              "https://www.rcalinkdummy.com/somelink",
 }
 
 func TestEventFromSite247(t *testing.T) {
